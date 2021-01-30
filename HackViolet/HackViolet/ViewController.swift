b9f1fc9ace363442a0f064082cd7a479afce10ae
Ananya <jajooananya@gmail.com>
2021-01-30 13:26:51 -0500

    home func.

--- HackViolet/HackViolet/ViewController.swift
+++ HackViolet/HackViolet/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var intrestField: UITextField!
+    
+    var name: String!
+    var age: String!
+    var intrest: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //name = nameField.text
+        age = ageField.text
+        intrest = intrestField.text
+        name = nameField.text
+        print(nameField.text)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let uvProfile = segue.destination as? profileViewController{
+            uvProfile.name = self.name
+            uvProfile.age = self.age
+            uvProfile.intrest = self.intrest
+        }
+        
+    }
+    
+    @IBAction func doneAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "infoToHome", sender: self)
+    }
+    
 
 }
 
